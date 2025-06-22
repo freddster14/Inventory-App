ceaf0fe4d70bcb203b10650d6f9a864b2e8dbf94
freddster14 <freddycarrillo951@gmail.com>
2025-06-22 16:53:03 -0500

    Add item routing and views for item display, fix item quantity rendering

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const favicon = require('serve-favicon');
 const mainRouter = require('./routes/main');
 const categoryRouter = require('./routes/categoryRouter');
+const itemRouter = require('./routes/itemRouter');
 
 const app = express();
 const PORT = 3000;
@@ -12,6 +13,7 @@ app.set('view engine', 'ejs');
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(express.urlencoded({ extended: true }));
 app.use('/category', categoryRouter);
+app.use('/item', itemRouter);
 app.use('/', mainRouter);
 
 app.listen(PORT);
--- controllers/itemController.js
+++ controllers/itemController.js
@@ -3,4 +3,5 @@ const db = require('../models/queries');
 exports.getItem = async (req, res) => {
   const { id } = req.query;
   const item = await db.getItem(id);
-}
+  res.render('item', { item });
+};
