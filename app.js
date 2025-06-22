adb789069a7ee1740faca9b4d07fcba000a5faec
freddster14 <freddycarrillo951@gmail.com>
2025-06-22 16:44:29 -0500

    Implement routing and views for category display

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
+const mainRouter = require('./routes/main');
+const categoryRouter = require('./routes/categoryRouter');
 
 const app = express();
 const PORT = 3000;
@@ -9,6 +11,7 @@ app.set('view engine', 'ejs');
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(express.urlencoded({ extended: true }));
-app.use('/', (req, res) => res.send('working'));
+app.use('/category', categoryRouter);
+app.use('/', mainRouter);
 
 app.listen(PORT);
