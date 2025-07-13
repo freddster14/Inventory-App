f598e8f90c098f48c3e96d7192a4faeb301b2c72
freddster14 <freddycarrillo951@gmail.com>
2025-07-13 17:26:25 -0500

    Add styles and layout enhancements: create homepage and main CSS files, implement sidebar and update topbar layout, and configure static assets in app.js.

--- app.js
+++ app.js
@@ -7,10 +7,13 @@ const itemRouter = require('./routes/itemRouter');
 
 const app = express();
 const PORT = 3000;
+const assestsPath = path.join(__dirname, 'public/styles');
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(express.static(assestsPath));
 app.use(express.urlencoded({ extended: true }));
 app.use('/category', categoryRouter);
 app.use('/item', itemRouter);
