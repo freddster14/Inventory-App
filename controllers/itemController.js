282ec62a10eb768d17e9144a1393413bb4ca7f25
freddster14 <freddycarrillo951@gmail.com>
2025-10-28 15:27:03 -0500

    Refactor itemController to utilize query helper functions for database operations

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -2,15 +2,16 @@ const db = require('../models/queries');
 const { validationResult } = require('express-validator');
 const { validateCategory, validateInfo, validateName } = require('../models/validators');
 const { buildUrl } = require('../models/helper-functions');
+const { getCount, queryAll, getPagination, queryOne, execute, updateById, insertOne } = require('../models/query-helpers');
 
 exports.getItems = async (req, res) => {
   const page = parseInt(req.query.page, 10) || 1;
   const limit = parseInt(req.query.limit, 10) || 12;
-  const totalPages = await db.getTotalPages(limit, 'items');
+  const totalPages = Math.ceil(await getCount('items') / limit);
   // Revert back to max page as a result of limit change or data
   if (totalPages < page) return res.redirect(`/item?page=${totalPages}&limit=${limit}`);
   const fn = (newQuery, query) => buildUrl(req, newQuery, 'item', query);
-  const items = await db.getItems(limit, page);
+  const items = await getPagination('SELECT * FROM items LIMIT $1 OFFSET $2', [], limit, page);
   return res.render('item/default', {
     items,
     page,
@@ -22,25 +23,25 @@ exports.getItems = async (req, res) => {
 
 exports.getItem = async (req, res) => {
   const { id } = req.params;
-  const item = await db.getItem(id);
-  const category = await db.getCategory(item.cat_id);
+  const item = await queryOne('SELECT * FROM items WHERE id=$1', [id]);
+  const category = await queryOne('SELECT * FROM categories WHERE id=$1', [item.cat_id]);
   res.render('item/view', { item, category });
 };
 
 exports.getItemForm = async (req, res) => {
   const { id } = req.params;
-  const categories = await db.getCategories();
+  const categories = await queryAll('SELECT * FROM categories WHERE id>1');
   let selectedCategory = null;
   if (id) {
-    selectedCategory = await db.getCategory(id);
+    selectedCategory = await queryOne('SELECT * FROM categories WHERE id=$1', [id]);
   }
   res.render('item/form', { categories, selectedCategory, errors: [] });
 };
 
 exports.getUpdateForm = async (req, res) => {
   const { id } = req.params;
-  const item = await db.getItem(id);
-  const category = await db.getCategory(item.cat_id);
+  const item = await queryOne('SELECT * FROM items WHERE id=$1', [id]);
+  const category = await queryOne('SELECT * FROM categories WHERE id=$1', [item.cat_id]);
   res.render('item/edit', {
     item,
     category,
@@ -50,15 +51,15 @@ exports.getUpdateForm = async (req, res) => {
 
 exports.getDelete = async (req, res) => {
   const { id } = req.params;
-  const item = await db.getItem(id);
-  const category = await db.getCategory(item.cat_id);
+  const item = await queryOne('SELECT * FROM items WHERE id=$1', [id]);
+  const category = await queryOne('SELECT * FROM categories WHERE id=$1', [item.cat_id]);
   res.render('item/delete', { item, category });
 };
 
 exports.deleteItem = async (req, res) => {
   const { id } = req.params;
-  const item = await db.getItem(id);
-  await db.deleteItem(id);
+  const item = await queryOne('SELECT * FROM items WHERE id=$1', [id]);
+  await execute('DELETE FROM items WHERE id=$1', [id]);
   res.redirect(`/category/${item.cat_id}`);
 };
 
@@ -70,10 +71,10 @@ exports.postItem = [
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       const { id } = req.params;
-      const categories = await db.getCategories();
+      const categories = await queryAll('SELECT * FROM categories WHERE id>1');
       let selectedCategory = null;
       if (id) {
-        selectedCategory = await db.getCategory(id);
+        selectedCategory = await queryOne('SELECT * FROM categories WHERE id=$1', [id]);
       }
       return res.status(400).render('item/form', {
         categories,
@@ -89,14 +90,20 @@ exports.postItem = [
       quantity,
     } = req.body;
     try {
-      await db.postItem(catId, name, info, price, quantity);
+      await insertOne('items', {
+        cat_id: catId,
+        name,
+        info,
+        price,
+        quantity,
+      });
       return res.redirect(`/category/${catId}`);
     } catch (error) {
       const { id } = req.params;
-      const categories = await db.getCategories();
+      const categories = await queryAll('SELECT * FROM categories WHERE id>1');
       let selectedCategory = null;
       if (id) {
-        selectedCategory = await db.getCategory(id);
+        selectedCategory = await queryOne('SELECT * FROM categories WHERE id=$1', [id])
       }
       return res.render('item/form', {
         categories,
@@ -120,7 +127,7 @@ exports.updateItem = [
       quantity,
     } = req.body;
     const sameCat = { id: catId, name: category };
-    const item = await db.getItem(id);
+    const item = await queryOne('SELECT * FROM items WHERE id=$1', [id]);
     if (!errors.isEmpty()) {
       return res.status(400).render('item/edit', {
         item,
@@ -129,7 +136,7 @@ exports.updateItem = [
       });
     }
     try {
-      await db.updateItem(item, info, price, quantity);
+      await updateById('items', item.id, { info, price, quantity });
       return res.redirect(`/item/${item.id}`);
     } catch (error) {
       return res.render('item/edit', {
